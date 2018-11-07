1dbf966f4d12c10458f82f680ee56118b5547e9d
Sara Saleem <8222963+ssaleem@users.noreply.github.com>
2018-11-07 13:55:18 -0500

    Add gulp for CSS tasks

---
+++ gulpfile.js
@@ -0,0 +1,21 @@
+var gulp         = require('gulp'),
+    cssnano      = require('gulp-cssnano'),
+    sass         = require('gulp-sass');
+
+var supported = [
+    'last 2 versions',
+    'safari >= 8',
+    'ie >= 10',
+    'ff >= 20',
+    'ios 6',
+    'android 4'
+];
+
+gulp.task('css', function(){
+    return gulp.src(['sass/**/*.scss'])
+        .pipe(sass())
+        .pipe(cssnano({
+            autoprefixer: {browsers: supported, add: true}
+        }))
+        .pipe(gulp.dest('css'));
+});
